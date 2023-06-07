54d3e5edf72082b5a59b1157e20c04a8993481e9
Joseph <yosephassefa85@gmail.com>
2023-06-07 23:59:11 +0300

    Vechile service age crud operation done

---
+++ RadmsWebAPI/Models/PutModels/VehicleServiceAgeLookupUpdateModel.cs
@@ -0,0 +1,35 @@
+﻿using RadmsEntities;
+
+namespace RadmsWebAPI.Models.PutModels
+{
+    public class VehicleServiceAgeLookupUpdateModel
+    {
+        public VehicleServiceAgeLookupUpdateModel()
+        {
+            //   VehicleDetailsTransactions = new HashSet<VehicleDetailsTransactionEntity>();
+
+        }
+
+
+        public int VehicleServiceAgeId { get; set; }
+
+        public string VehicleServiceName { get; set; } = null!;
+
+        //  public virtual ICollection<VehicleDetailsTransactionEntity> VehicleDetailsTransactions { get; set; }
+
+        public VehicleServiceAgeLookupUpdateModel(VehicleServiceAgeLookupEntity model)
+        {
+            this.VehicleServiceAgeId = model.VehicleServiceAgeId;
+            this.VehicleServiceName = model.VehicleServiceName;
+            //   VehicleDetailsTransactions = new HashSet<VehicleDetailsTransactionEntity>();
+
+        }
+        public T MapToViewEntity<T>() where T : class
+        {
+            VehicleServiceAgeLookupEntity entity = new VehicleServiceAgeLookupEntity();
+            entity.VehicleServiceAgeId = this.VehicleServiceAgeId;
+            entity.VehicleServiceName = this.VehicleServiceName;
+            return entity as T;
+        }
+    }
+}
