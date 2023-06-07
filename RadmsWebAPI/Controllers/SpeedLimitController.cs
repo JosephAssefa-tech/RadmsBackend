ee1bb0faa0cbbf91618586be31121ed5d5338e14
Joseph <yosephassefa85@gmail.com>
2023-06-08 00:06:24 +0300

    Speed limit CRUD operation done

--- RadmsWebAPI/Controllers/SpeedLimitController.cs
+++ RadmsWebAPI/Controllers/SpeedLimitController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RadmsEntities;
 using RadmsServiceFacade;
+using RadmsWebAPI.Models.PostModels;
+using RadmsWebAPI.Models.PutModels;
 using RadmsWebAPI.Models.ViewModels;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -18,7 +20,7 @@ namespace RadmsWebAPI.Controllers
         }
         // GET: api/<SpeedLimitController>
         [HttpGet]
-        public List<SpeedLimitLookupViewModel> GetAll(string language)
+        public List<SpeedLimitLookupViewModel> GetAll(string? language)
         {
             List<SpeedLimitLookupEntity> entities = this._service.GetAll(language);
             List<SpeedLimitLookupViewModel> viewModels = new List<SpeedLimitLookupViewModel>();
@@ -39,20 +41,40 @@ namespace RadmsWebAPI.Controllers
 
         // POST api/<SpeedLimitController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Save([FromBody] SpeedLimitLookupPostModel viewModel)
         {
+            string result = this._service.Save(viewModel.MapToViewEntity<SpeedLimitLookupEntity>());
+            if (result == "saved sucessfuly")
+            {
+
+            }
+            else
+            {
+
+            }
+
         }
 
         // PUT api/<SpeedLimitController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut]
+        public void Update([FromBody] SpeedLimitLookupUpdateModel viewModel)
         {
+            string result = this._service.Update(viewModel.MapToViewEntity<SpeedLimitLookupEntity>());
+            if (result == "Updated sucessfuly")
+            {
+
+            }
+            else
+            {
+
+            }
         }
 
         // DELETE api/<SpeedLimitController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpDelete]
+        public void Delete(int speedLimitId)
         {
+            var result = this._service.Delete(speedLimitId);
         }
     }
 }
