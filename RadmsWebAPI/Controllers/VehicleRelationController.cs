850d0e2da2d4fbf5d3904a9440cf6a6ce187e91f
Joseph <yosephassefa85@gmail.com>
2023-06-07 23:30:44 +0300

    VechileRelation CRUD operation done

--- RadmsWebAPI/Controllers/VehicleRelationController.cs
+++ RadmsWebAPI/Controllers/VehicleRelationController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RadmsEntities;
 using RadmsServiceFacade;
+using RadmsWebAPI.Models.PostModels;
 using RadmsWebAPI.Models.ViewModels;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -41,20 +42,40 @@ namespace RadmsWebAPI.Controllers
 
         // POST api/<VehicleRelationController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Save([FromBody] VehicleRelationLookupPostModel viewModel)
         {
+            string result = this._service.Save(viewModel.MapToViewEntity<VehicleRelationLookupEntity>());
+            if (result == "saved sucessfuly")
+            {
+
+            }
+            else
+            {
+
+            }
+
         }
 
         // PUT api/<VehicleRelationController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut]
+        public void Update([FromBody] VehicleRelationLookupUpdateModel viewModel)
         {
+            string result = this._service.Update(viewModel.MapToViewEntity<VehicleRelationLookupEntity>());
+            if (result == "Updated sucessfuly")
+            {
+
+            }
+            else
+            {
+
+            }
         }
 
         // DELETE api/<VehicleRelationController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpDelete]
+        public void Delete(int vehicleRelationId)
         {
+            var result = this._service.Delete(vehicleRelationId);
         }
     }
 }
