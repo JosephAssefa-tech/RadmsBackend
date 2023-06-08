bb2693f2ea594d2c91be364a0a38153a62a5a229
Joseph <yosephassefa85@gmail.com>
2023-06-08 17:26:31 +0300

    pavemntType,Pedesttrian movement and seatype type facade layer interface and implementation done for crud operation

--- RadmsServiceManager/PaymentTypeService.cs
+++ RadmsServiceManager/PaymentTypeService.cs
@@ -18,10 +18,104 @@ namespace RadmsServiceManager
             _repositiory = repositiory;
 
         }
-        public List<PavementTypeLookupEntity> GetAll(string language)
+
+        public string Delete(int pavementTypeId)
+        {
+            if (pavementTypeId != 0)
+            {
+                var result = _repositiory.Delete(pavementTypeId);
+
+                return "sucessfuly deleted";
+
+            }
+            else
+            {
+                return "can't delete b/c accident cause id is not given";
+            }
+        }
+
+        public List<PavementTypeLookupEntity> GetAll(string? language)
         {
             List<PavementTypeLookupEntity> results = this._repositiory.GetAll(language);
             return results;
+        }
+        private string Validate(PavementTypeLookupEntity entity)
+        {
+            if (entity.PavementName == String.Empty)
+            {
+                return "PavementName  name can not be empty";
+            }
+            //else if(entity.startDate>entity.endDate)
+            //{
+            //    return "start date can't be greater than end date";
+            //}
+            else
+            {
+                return string.Empty;
+            }
+
+        }
+
+        public string Save(PavementTypeLookupEntity entity)
+        {
+            try
+            {
+                string msg = Validate(entity);
+                if (msg != String.Empty)
+                {
+                    return msg;
+                }
+                else
+                {
+                    bool result = _repositiory.Save(entity);
+                    if (result == true)
+                    {
+                        return "saved sucessfuly";
+                    }
+                    else
+                    {
+                        return "unkown error occured";
+
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+
+            }
+        }
+
+        public string Update(PavementTypeLookupEntity entity)
+        {
+            try
+            {
+                string msg = Validate(entity);
+                if (msg != String.Empty)
+                {
+                    return msg;
+                }
+                else
+                {
+                    bool result = _repositiory.Update(entity);
+                    if (result == true)
+                    {
+                        return "Updated sucessfuly";
+                    }
+                    else
+                    {
+                        return "unkown error occured";
+
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+
+            }
         }
     }
 }
--- RadmsServiceManager/PedestrianMovementLookupServices.cs
+++ RadmsServiceManager/PedestrianMovementLookupServices.cs
@@ -18,10 +18,104 @@ namespace RadmsServiceManager
             _repositiory = repositiory;
 
         }
-        public List<PedestrianMovementLookupEntity> GetAll()
+
+        public string Delete(int pedestrianMovementId)
         {
-            List<PedestrianMovementLookupEntity> results = this._repositiory.GetAll();
+            if (pedestrianMovementId != 0)
+            {
+                var result = _repositiory.Delete(pedestrianMovementId);
+
+                return "sucessfuly deleted";
+
+            }
+            else
+            {
+                return "can't delete b/c accident cause id is not given";
+            }
+        }
+
+        public List<PedestrianMovementLookupEntity> GetAll(string? language)
+        {
+            List<PedestrianMovementLookupEntity> results = this._repositiory.GetAll(language);
             return results;
+        }
+        private string Validate(PedestrianMovementLookupEntity entity)
+        {
+            if (entity.PedestrianMovementName == String.Empty)
+            {
+                return "PedestrianMovementName  name can not be empty";
+            }
+            //else if(entity.startDate>entity.endDate)
+            //{
+            //    return "start date can't be greater than end date";
+            //}
+            else
+            {
+                return string.Empty;
+            }
+
+        }
+        public string Save(PedestrianMovementLookupEntity entity)
+        {
+
+            try
+            {
+                string msg = Validate(entity);
+                if (msg != String.Empty)
+                {
+                    return msg;
+                }
+                else
+                {
+                    bool result = _repositiory.Save(entity);
+                    if (result == true)
+                    {
+                        return "saved sucessfuly";
+                    }
+                    else
+                    {
+                        return "unkown error occured";
+
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+
+            }
+        }
+
+        public string Update(PedestrianMovementLookupEntity entity)
+        {
+            try
+            {
+                string msg = Validate(entity);
+                if (msg != String.Empty)
+                {
+                    return msg;
+                }
+                else
+                {
+                    bool result = _repositiory.Update(entity);
+                    if (result == true)
+                    {
+                        return "Updated sucessfuly";
+                    }
+                    else
+                    {
+                        return "unkown error occured";
+
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+
+            }
         }
     }
 }
